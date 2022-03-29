632464142eb75742b0c578dcb9f93e57dc37cbf9
jiceberg <jisenberg3@gatech.edu>
2022-03-29 10:32:33 -0400

    add agglomerative clustering

--- src/clustering.py
+++ src/clustering.py
@@ -4,9 +4,10 @@ import os
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, AgglomerativeClustering
 from sklearn.decomposition import PCA
 
+IMAGES_PATH = "data/images"
 
 class Clustering:
     def __init__(self, K=5, num_clusters=8):
@@ -50,30 +51,59 @@ class Clustering:
         return np.load(save_file_path, encoding='bytes')
 
     def __generate_images(self, u_subdir=""):
-        for subdir, dirs, files in os.walk(os.path.join("data/images", u_subdir)):
+        all_images = []
+        total_num_files = 0
+        for subdir, dirs, files in os.walk(os.path.join(IMAGES_PATH, u_subdir)):
             files = [ fi for fi in files if fi.endswith(".jpg") ]
             imgs = np.zeros((len(files), 1000, 1000, 3))
             i = 0
             for file in files:
                 imgs[i, ...] = self.__compress_image(subdir, file)
                 i += 1
-        return imgs
+            total_num_files += i
+            all_images.append((imgs, subdir))
+        return all_images, total_num_files
 
-    def cluster_images(self, u_subdir="", display = False):
-        original_images = self.__generate_images(u_subdir=u_subdir)
-        images = original_images.reshape(len(original_images), -1)
+    def cluster_images_kmeans(self, u_subdir=""):
+        all_images_path = os.path.join(IMAGES_PATH, u_subdir, "all_images.npy")
+        if not os.path.exists(all_images_path):
+            original_images, total_num_images = self.__generate_images(u_subdir=u_subdir)
+
+            all_images = np.zeros((total_num_images, 1000 * 1000 * 3))
+
+            start = 0
+            for image_subdir, subdir in original_images:
+                end = start + len(image_subdir)
+                all_images[start:end] = image_subdir.reshape(len(image_subdir), -1)
+                start = end
+        
+            np.save(all_images_path, all_images)
+
+        images = np.load(all_images_path)
         kmeans = KMeans(n_clusters=self.num_clusters)
+
         kmeans.fit(images)
 
-        labels = kmeans.labels_
+        return kmeans.labels_
+    
+    def cluster_images_agglomerate(self, u_subdir=""):
+        all_images_path = os.path.join(IMAGES_PATH, u_subdir, "all_images.npy")
+        if not os.path.exists(all_images_path):
+            original_images, total_num_images = self.__generate_images(u_subdir=u_subdir)
 
-        if display:
-            fig = plt.figure(figsize=(1, len(images)))
-            i = 0
-            for img in original_images:
-                ax = fig.add_subplot(1, len(images), i + 1, xticks=[], yticks=[])
-                ax.imshow(img)
-                ax.set_title(f'{labels[i]}')
-                i += 1
-            
-            plt.show()
+            all_images = np.zeros((total_num_images, 1000 * 1000 * 3))
+
+            start = 0
+            for image_subdir, subdir in original_images:
+                end = start + len(image_subdir)
+                all_images[start:end] = image_subdir.reshape(len(image_subdir), -1)
+                start = end
+        
+            np.save(all_images_path, all_images)
+
+        images = np.load(all_images_path)
+        c = AgglomerativeClustering(n_clusters=self.num_clusters)
+
+        c.fit(images)
+
+        return c.labels_
--- src/main.py
+++ src/main.py
@@ -1,4 +1,30 @@
-from clustering import Clustering
+import os
 
-clustering = Clustering()
-clustering.cluster_images(u_subdir="020", display = True)
+import matplotlib.pyplot as plt
+import numpy as np
+
+from clustering import IMAGES_PATH, Clustering
+
+clustering = Clustering(num_clusters=10)
+subdir = "020"
+labels_a = clustering.cluster_images_agglomerate(u_subdir=subdir)
+# labels_k = clustering.cluster_images_kmeans(u_subdir=subdir)
+
+images = np.load(os.path.join(IMAGES_PATH, subdir, "all_images.npy"))
+images = images.reshape(len(images), 1000, 1000, 3)
+
+fig = plt.figure(figsize=(2, len(images)))
+i = 0
+for img in images:
+    ax = fig.add_subplot(2, len(images), i + 1, xticks=[], yticks=[])
+    ax.imshow(img)
+    ax.set_title(f'{labels_a[i]}')
+    i += 1
+
+# for img in images:
+#     ax = fig.add_subplot(2, len(images), i + 1, xticks=[], yticks=[])
+#     ax.imshow(img)
+#     ax.set_title(f'{labels_k[i - len(images)]}')
+#     i += 1
+
+plt.show()
