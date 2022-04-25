e29d13e0f5310056585220af99dfc2b6b6616172
Adithya Vasudev <adithya.gvasudev@gmail.com>
2022-04-24 21:25:21 -0400

    ready for NN

--- src/neural_net.py
+++ src/neural_net.py
@@ -3,11 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch
  
-X = pd.read_csv("data/training/dataset.csv")[['graphical_appearance_no', 'colour_group_code', 'cluster']] # you can add more features
-y = pd.read_csv("data/training/dataset.csv")[['purchased']] # modify this to your target variable
+X = pd.read_csv("data/training/articles_subset.csv")[['product_type_no', 'graphical_appearance_no', 'colour_group_code', 'perceived_colour_value_id', 'perceived_colour_master_id', 'department_no', 'index_code', 'index_group_no', 'section_no', 'garment_group_no']] # you can add more features
+y = pd.read_csv("data/training/articles_subset.csv")[['cluster']] # modify this to your target variable
 X = X.to_numpy()
 y = y.to_numpy()
 
+for i in range(len(X[:, 6])):
+    X[i, 6] = ord(X[i, 6]) - ord('A')
+ 
 X = X.astype(np.float32)
 y = y.astype(np.float32)
 
