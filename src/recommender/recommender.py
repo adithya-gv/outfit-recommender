efa994ff133fcabd66d1fd011e3d9e8619c20a00
Rishabh Jain <r.jain.voyager@gmail.com>
2022-04-26 18:37:14 -0400

    input and display images from kNN

--- src/recommender/recommender.py
+++ src/recommender/recommender.py
@@ -2,6 +2,9 @@ from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+articles = pd.read_csv("data/articles.csv")[['article_id']].to_numpy() # the entire images data file
 
 def train():
     X = pd.read_csv("data/training/articles_subset.csv")[['product_type_no', 'graphical_appearance_no', 'colour_group_code', 'perceived_colour_value_id', 'perceived_colour_master_id', 'department_no', 'index_code', 'index_group_no', 'section_no', 'garment_group_no']] # you can add more features
@@ -29,3 +32,25 @@ def recommend(X):
     
     model = train()
     return model.kneighbors(X)
+
+"""
+Takes in a list of indexes and displays all the images that they correspond to
+"""
+def show_image(indexes):
+    fig = plt.figure()
+    for i, index in enumerate(indexes):
+        id = "0" + str(articles[index][0])
+        input_img = mpimg.imread("data/images/" + id[0:3] + "/" + id + ".jpg")
+        a = fig.add_subplot(2, 3, i+1)
+        imgplot = plt.imshow(input_img)
+    plt.show()
+
+
+input_index = [140] #this is the input image that you want to find similar items to 
+input = pd.read_csv("data/articles.csv")[['product_type_no', 'graphical_appearance_no', 'colour_group_code', 'perceived_colour_value_id', 'perceived_colour_master_id', 'department_no', 'index_code', 'index_group_no', 'section_no', 'garment_group_no']].to_numpy()[input_index[0]]
+input = input.reshape((1, input.shape[0]))
+show_image(input_index) # displays the input image
+
+indexes = recommend(input)
+output_indexes = indexes[1][0]
+show_image(output_indexes) # displays all 5 output images
